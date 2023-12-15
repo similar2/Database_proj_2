bf7e78850baa59eea095b809dc46b0c880ca0c74
similar2 <113870101+similar2@users.noreply.github.com>
2023-12-15 23:49:34 +0800

    Delete DDL_PostVideoReq.sql

--- DDL_PostVideoReq.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE if not exists PostVideoReq (
-    title VARCHAR(255), -- title of the video
-    description TEXT, -- description of the video
-    duration INT, -- duration of the video in seconds
-    publicTime TIMESTAMP -- scheduled public time of the video
-);
