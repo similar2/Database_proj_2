d63e8f9764e5493b1a6d85acff3c8b43f804aad9
similar2 <113870101+similar2@users.noreply.github.com>
2023-12-25 14:54:28 +0800

    Add files via upload

--- sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
+++ sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
@@ -57,32 +57,55 @@ public class DatabaseServiceImpl implements DatabaseService {
                     INSERT INTO likes (BV_liked, mid_liked)
                     VALUES (?, ?);""";
             String sql_sync_2_userinforeq = """
+                                    
+                    -- 创建包含所有 follower-followed 对的临时表
                     WITH FollowersMapping AS (SELECT unnest(ur.following) AS followed_mid,
                                                      ur.mid               AS follower_mid
                                               FROM UserRecord ur),
+                    -- 为每个 followed_mid 聚合其 followers
                          AggregatedFollowers AS (SELECT followed_mid,
                                                         ARRAY_AGG(DISTINCT follower_mid) AS followers
                                                  FROM FollowersMapping
                                                  GROUP BY followed_mid),
+                    -- 为每个用户聚合其 watched 列表
                          AggregatedWatched AS (SELECT vr.mid,
                                                       ARRAY_AGG(DISTINCT vr.bv) AS watched_videos
                                                FROM ViewRecord vr
-                                               GROUP BY vr.mid)
+                                               GROUP BY vr.mid),
+                    -- 为每个用户聚合其 liked 列表
+                         AggregatedLiked AS (SELECT lr.mid_liked                    AS mid,
+                                                    ARRAY_AGG(DISTINCT lr.BV_liked) AS liked_videos
+                                             FROM likes lr
+                                             GROUP BY lr.mid_liked),
+                    -- 为每个用户聚合其 favorited 列表
+                         AggregatedFavorites AS (SELECT fr.mid_favorite                    AS mid,
+                                                        ARRAY_AGG(DISTINCT fr.BV_favorite) AS favorited_videos
+                                                 FROM favorites fr
+                                                 GROUP BY fr.mid_favorite),
+                    -- 为每个用户聚合其 posted 列表
+                         AggregatedPosted AS (SELECT vr.ownerMid               AS mid,
+                                                     ARRAY_AGG(DISTINCT vr.bv) AS posted_videos
+                                              FROM VideoRecord vr
+                                              GROUP BY vr.ownerMid)
+                    -- 插入新记录到 UserInfoResp 表
                     INSERT
                     INTO UserInfoResp (mid, coin, following, follower, watched, liked, collected, posted)
                     SELECT ur.mid,
-                           ur.coin,
+                           ur.coin,                                                  -- 从 UserRecord 获取 coin 值
                            ur.following,
-                           COALESCE(af.followers, ARRAY []::bigint[]),
-                           COALESCE(aw.watched_videos, ARRAY []::VARCHAR(255)[]),
-                           ARRAY []::VARCHAR(255)[],
-                           ARRAY []::VARCHAR(255)[],
-                           ARRAY []::VARCHAR(255)[]
+                           COALESCE(af.followers, ARRAY []::bigint[]),               -- 如果有 followers，则插入，否则插入空数组
+                           COALESCE(aw.watched_videos, ARRAY []::VARCHAR(255)[]),    -- 如果有 watched_videos，则插入，否则插入空数组
+                           COALESCE(al.liked_videos, ARRAY []::VARCHAR(255)[]),      -- 如果有 liked_videos，则插入，否则插入空数组
+                           COALESCE(afv.favorited_videos, ARRAY []::VARCHAR(255)[]), -- 如果有 favorited_videos，则插入，否则插入空数组
+                           COALESCE(ap.posted_videos, ARRAY []::VARCHAR(255)[])      -- 如果有 posted_videos，则插入，否则插入空数组
                     FROM UserRecord ur
                              LEFT JOIN AggregatedFollowers af ON ur.mid = af.followed_mid
                              LEFT JOIN AggregatedWatched aw ON ur.mid = aw.mid
+                             LEFT JOIN AggregatedLiked al ON ur.mid = al.mid
+                             LEFT JOIN AggregatedFavorites afv ON ur.mid = afv.mid
+                             LEFT JOIN AggregatedPosted ap ON ur.mid = ap.mid
                     WHERE NOT EXISTS (SELECT 1 FROM UserInfoResp WHERE mid = ur.mid);
-                                   
+                                  
                     """;
             String sql_sync_2_authinfo = """              
                     INSERT INTO AuthInfo (mid, password, qq, wechat)
@@ -165,6 +188,8 @@ public class DatabaseServiceImpl implements DatabaseService {
                 }
                 stmt_user.executeBatch();
                 stmt_user.close();
+                sync.execute(sql_sync_2_userinforeq);
+                sync.execute(sql_sync_2_authinfo);
 
                 cnt = 0;
                 int cnt_view = 0;
@@ -308,8 +333,7 @@ public class DatabaseServiceImpl implements DatabaseService {
                     }
                 }
                 stmt_danmu.executeBatch();
-                sync.execute(sql_sync_2_userinforeq);
-                sync.execute(sql_sync_2_authinfo);
+
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
